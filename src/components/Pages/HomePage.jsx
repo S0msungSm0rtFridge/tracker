b033ee389652f8bfe69591fe827aa8e3c61c8230
Nelson <nelson.chan@stonybrook.edu>
2025-08-29 21:39:47 -0400

    starting

---
+++ src/components/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function Homepage() {
+    
+    return (
+        <div className="container text-center p-5 bg-light rounded">
+            <h1 className="text-primary">Test</h1>
+            <p className="lead">This is styled with Bootstrap</p>
+        </div>
+    );
+}
+
+export { Homepage };
